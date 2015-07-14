b42664f84ab4565e180dc4456a004c1abb6f54cc
J.P. Weiser <jon@jpweiser.com>
2015-07-13 17:48:51 -0700

    Adds functionality for cutting bytes and characters

---
+++ cuts/bytes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" bytes.py is part of cuts
+This module provides the ByteCutter object.
+"""
+from cuts.cutter import Cutter
+
+class ByteCutter(Cutter) :
+    """Cuts line into specified bytes based on input delimiter.
+    Arguments:
+        bts -          List that specifies which bytes to return in which order
+        separator -    Output delimiter. (default '')
+        no_field -     Determines output if invalid field position is specified
+    """
+    def __init__(self,bts,separator='',no_field=''):
+        super(ByteCutter,self).__init__(bts,separator,invalid_pos=no_field)
+
+    def _line(self,line):
+        """Returns line untouched, expected to be byte array.
+
+        Argument:
+            line - Input line to cut
+        """
+        return line
