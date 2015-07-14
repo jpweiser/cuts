b9b08b5a2a57a300c8183006e706a1529c793883
J.P. Weiser <jon@jpweiser.com>
2015-07-13 17:49:23 -0700

    Adds functionality for cutting bytes and characters

---
+++ cuts/chars.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" chars.py is part of cuts
+This module provides the CharCutter object.
+"""
+from cuts.cutter import Cutter
+
+class CharCutter(Cutter):
+    """Cuts line into specified characters based on input delimiter.
+    Arguments:
+        chars -        List that specifies which chars to return in which order
+        separator -    Output delimiter. (default '')
+        no_field -     Determines output if invalid field position is specified
+    """
+    def __init__(self,chars,separator='',no_field=''):
+        super(CharCutter,self).__init__(chars,separator,invalid_pos=no_field)
+
+    def _line(self,line):
+        """Returns string with trailing whitespace characters removed
+
+        Argument:
+            line - Input line to cut
+        """
+        return line.rstrip()
